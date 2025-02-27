a83c0246c0e859049662b68914161f89415eb479
John_jetsada <jornny.prombut@gmail.com>
2025-02-27 20:56:49 +0700

    Create MVC

---
+++ includes/db.php
@@ -0,0 +1,20 @@
+<?php
+
+function getConnection():mysqli
+{
+    $hostname = 'localhost';
+    $dbName = 'project_web';
+    $username = 'project_web';
+    $password = '123***';
+    $conn = new mysqli($hostname, $username, $password, $dbName);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else
+    {
+        echo 'Connection completed';
+    }
+    return $conn;
+}
+
+require_once DATABASE_DIR . '/users.php';
