183e21086aa144fc8ef6973f6664769ee767417d
natta007 <la1329901310097@gmail.com>
2025-03-20 02:25:23 +0700

    Upevent

--- templates/detail_event_get.php
+++ templates/detail_event_get.php
@@ -23,7 +23,7 @@ $num_images = ($images_result) ? $images_result->num_rows : 0;
                     <?php endif; ?>
                 </div>
 
-              
+                
                 <button class="carousel-control-prev" type="button" data-bs-target="#eventImageCarousel" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
